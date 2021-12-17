47eaf76056abf52a0bc66dcc54815c14cd8bdab5
Szymon Kupiecki <kupieckiszymon@gmail.com>
2021-12-17 22:42:47 +0100

    dodanie przykładowych testów

--- test/main_gtest.cpp
+++ test/main_gtest.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include "testing_tests.cpp"
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
--- test/testing_tests.cpp
+++ test/testing_tests.cpp
@@ -1,4 +1,9 @@
-//
-// Created by Szymon on 15.12.2021.
-//
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
+TEST(testing_test, always_true){
+    EXPECT_EQ("test","test");
+}
+//TEST(testing_test, always_false){
+//    EXPECT_EQ("test","test1");
+//}
