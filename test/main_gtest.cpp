7c5f90fd6729e665dadcb9a8610acdb441b83550
Szymon Kupiecki <kupieckiszymon@gmail.com>
2021-12-17 23:39:03 +0100

    dodanie testów z ZPO wiki

--- test/main_gtest.cpp
+++ test/main_gtest.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
-#include "testing_tests.cpp"
+#include "testing_tests.cpp" //dodawanie nieefektywne ale umozliwia niedodawanie niechcianych testow
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
