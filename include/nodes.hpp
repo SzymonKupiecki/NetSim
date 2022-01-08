4eeab8b01841737b3ddf2c7f8871b0a9abe6d60e
kk2115 <80417519+kk2115@users.noreply.github.com>
2022-01-08 22:27:08 +0100

    nie przetestowana klasa ReciverPreferences

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -2,6 +2,7 @@
 #define NODES_H
 
 #include <map>
+#include <utility>
 #include <vector>
 #include <memory>
 #include "helpers.hpp"
@@ -11,19 +12,25 @@ class IPackageReceiver{
 };
 
 class ReciverPreferences{
-// na diagramie UML wszystkie pola i metody maja byc publiczne
-public:
+// na diagramie UML wszystkie pola i metody maja byc publiczne, ale postanowilem ze pola zrobie prywatne
 
+public:
     using preferences_t = std::map<IPackageReceiver*, double>; // nie wiem po co te aliasy
     using const_iterator = preferences_t::const_iterator;
 
-    ReciverPreferences( ProbabilityGenerator pg){probability_value = pg; } //sprawdzic czy w konstruktorze nie ma tez byc preferencji
+    ReciverPreferences(ProbabilityGenerator pg){probability_value = std::move(pg); } //sprawdzic czy w konstruktorze nie ma tez byc preferencji
 
-    void add_receiver(IPackageReceiver* r); //TODO: implementacja
-    void remove_receiver(IPackageReceiver* r); //TODO: przy implementacji uwzglednic uwagi szymona
-    IPackageReceiver* choose_receiver(); //TODO: implementacja
-    const preferences_t& get_preferences(); //TODO: implementacja
+    [[nodiscard]] typename ReciverPreferences::const_iterator cbegin() const {return preferences.cbegin(); }
+    [[nodiscard]] typename ReciverPreferences::const_iterator cend() const {return preferences.cend(); }
+    [[nodiscard]] typename ReciverPreferences::const_iterator begin() const {return preferences.begin(); }
+    [[nodiscard]] typename ReciverPreferences::const_iterator end() const {return preferences.end(); }
 
+    void add_receiver(IPackageReceiver* r); //TODO: testy
+    void remove_receiver(IPackageReceiver* r); //TODO: testy
+    IPackageReceiver* choose_receiver(); //TODO: testy
+    const preferences_t& get_preferences(){ return preferences; }; //TODO: testy
+
+private: //TODO: czy zostawic prywatne
     preferences_t preferences; //sluzy do przechowywania odbiorcow i przawdopodobienstwa wybrania odbiorcy
     ProbabilityGenerator probability_value;  //"uchwyt" do funkcji zdefiniowanej w helpers do wyboru liczby losowej
 };
--- src/nodes.cpp
+++ src/nodes.cpp
@@ -1,18 +1,44 @@
 #include "nodes.hpp"
 
 void ReciverPreferences::add_receiver(IPackageReceiver* r){
+    // funkcja na razie ma nie zajmowac sie odbiorcami z konkretnym prawdopodobienstwem,
+    // tak samo w remove_receiver:
+    double prawdopodobienstwo = 1/( double(preferences.size()) + 1.0 );
+
+    for(auto &x: preferences){
+        x.second = prawdopodobienstwo;
+    }
+
+    preferences.insert(std::pair<IPackageReceiver*, double>(r, prawdopodobienstwo));
 }
 
 void ReciverPreferences::remove_receiver(IPackageReceiver* r){
+    //funkcja dziala rowniez gdy r nie ma w mapie preferences
+    if(preferences.find(r) != preferences.end()){
+        preferences.erase(r);
 
+        // funkcja na razie ma nie zajmowac sie odbiorcami z konkretnym prawdopodobienstwem, wiec:
+        double prawdopodobienstwo = 1/( double(preferences.size()));
+
+        for(auto &x: preferences){
+            x.second = prawdopodobienstwo;
+        }
+    }
 }
 
 IPackageReceiver* ReciverPreferences::choose_receiver(){
+    double random = probability_value(); //warosc od 0 do 1
+    double dystrybuanta_1 = 0;
+    double dystrybuanta_2 = 0;
 
-    return nullptr;
+    for(auto reciver: preferences){
+        dystrybuanta_1 = dystrybuanta_2;
+        dystrybuanta_2 += reciver.second;
+
+        if(dystrybuanta_1 < random and random < dystrybuanta_2){
+            return reciver.first;
+        }
+    }
+    return nullptr; //cos poszlo nie tak
 }
 
-const ReciverPreferences::preferences_t& ReciverPreferences::get_preferences(){
-
-    return <#initializer#>;
-}
