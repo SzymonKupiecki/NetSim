661717d5ca82225f13ea4923e28784fa3baec267
kk2115 <80417519+kk2115@users.noreply.github.com>
2022-01-08 11:59:44 +0100

    Kajtek Kups, poczatki pracy ReciverPreferences 2

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -11,13 +11,21 @@ class IPackageReceiver{
 };
 
 class ReciverPreferences{
+// na diagramie UML wszystkie pola i metody maja byc publiczne
+public:
+
     using preferences_t = std::map<IPackageReceiver*, double>; // nie wiem po co te aliasy
     using const_iterator = preferences_t::const_iterator;
 
+    ReciverPreferences( ProbabilityGenerator pg){probability_value = pg; } //sprawdzic czy w konstruktorze nie ma tez byc preferencji
 
-private:
+    void add_receiver(IPackageReceiver* r); //TODO: implementacja
+    void remove_receiver(IPackageReceiver* r); //TODO: przy implementacji uwzglednic uwagi szymona
+    IPackageReceiver* choose_receiver(); //TODO: implementacja
+    const preferences_t& get_preferences(); //TODO: implementacja
+
     preferences_t preferences; //sluzy do przechowywania odbiorcow i przawdopodobienstwa wybrania odbiorcy
-    ProbabilityGenerator probability_value = probability_generator;  //"uchwyt" do funkcji zdefiniowanej w helpers do wyboru liczby losowej
+    ProbabilityGenerator probability_value;  //"uchwyt" do funkcji zdefiniowanej w helpers do wyboru liczby losowej
 };
 
 #endif
---
+++ src/nodes.cpp
@@ -0,0 +1,18 @@
+#include "nodes.hpp"
+
+void ReciverPreferences::add_receiver(IPackageReceiver* r){
+}
+
+void ReciverPreferences::remove_receiver(IPackageReceiver* r){
+
+}
+
+IPackageReceiver* ReciverPreferences::choose_receiver(){
+
+    return nullptr;
+}
+
+const ReciverPreferences::preferences_t& ReciverPreferences::get_preferences(){
+
+    return <#initializer#>;
+}
