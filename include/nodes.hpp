d3ca450addb52b82e69b5405141c08d550a1d8a1
Szymon Kupiecki <kupieckiszymon@gmail.com>
2022-01-12 11:02:09 +0100

    zmiana wskaznika na obiekt probabilty generator

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -30,32 +30,32 @@ public:
     using preferences_t = std::map<IPackageReceiver*, double>;
     using const_iterator = preferences_t::const_iterator;
 
-    explicit ReceiverPreferences(ProbabilityGenerator* pg = &probability_generator): probability_value_(pg){} //sprawdzic czy w konstruktorze nie ma tez byc preferencji
+    explicit ReceiverPreferences(ProbabilityGenerator pg = probability_generator): probability_value_(pg){} //sprawdzic czy w konstruktorze nie ma tez byc preferencji
 
     [[nodiscard]] typename ReceiverPreferences::const_iterator cbegin() const {return preferences_.cbegin(); }
     [[nodiscard]] typename ReceiverPreferences::const_iterator cend() const {return preferences_.cend(); }
     [[nodiscard]] typename ReceiverPreferences::const_iterator begin() const {return preferences_.begin(); }
     [[nodiscard]] typename ReceiverPreferences::const_iterator end() const {return preferences_.end(); }
 
-    void add_receiver(IPackageReceiver* r); //TODO: testy
-    void remove_receiver(IPackageReceiver* r); //TODO: testy
-    IPackageReceiver* choose_receiver(); //TODO: testy
-    const preferences_t& get_preferences(){ return preferences_; }; //TODO: testy
+    void add_receiver(IPackageReceiver* r);
+    void remove_receiver(IPackageReceiver* r);
+    IPackageReceiver* choose_receiver();
+    const preferences_t& get_preferences(){ return preferences_; };
 
 protected: //TODO: czy zostawic prywatne
     preferences_t preferences_; //sluzy do przechowywania odbiorcow i przawdopodobienstwa wybrania odbiorcy
-    ProbabilityGenerator* probability_value_;  //"uchwyt" do funkcji zdefiniowanej w helpers do wyboru liczby losowej
+    ProbabilityGenerator probability_value_ = probability_generator;  //"uchwyt" do funkcji zdefiniowanej w helpers do wyboru liczby losowej
 };
 
 
 class PackageSender{
 public:
-    PackageSender(PackageSender&&) = default; //nie wiem po co
+    PackageSender(PackageSender&&) = default;
 
     PackageSender() = default;
 
-    void send_package(); //wysyla przetworzona wartosc do odbiorcy //TODO: implementacja/poprawnosc
-    void push_package(Package&& package){ sending_bufor_ = std::move(package); }; //sluzy do wstawiania wartosci do bufora //TODO: test
+    void send_package(); //wysyla przetworzona wartosc do odbiorcy
+    void push_package(Package&& package){ sending_bufor_ = std::move(package); }; //sluzy do wstawiania wartosci do bufora
     const std::optional<Package>& get_sending_buffer(){ return sending_bufor_; };
 
     ReceiverPreferences receiver_preferences_;
